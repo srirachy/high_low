e5f4976a49835f184882e166d1e16d5129089dc5
srirachy <caseyereyes@gmail.com>
2022-04-07 11:40:45 -0700

    add starting files

---
+++ src/components/HighLow.jsx
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react';
+import Button from './Button';
+//import titleimg from '../images/titleimg.png';
+
+//initialize state values
+
+//could add useeffects here if needed
+
+//methods
+
+const HighLow = () => {
+    return (
+        <>
+            <h1>test</h1>
+        </>
+    );
+};
+
+export default HighLow;
